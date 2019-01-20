2001d042941ee876d9519ac07d30e93825d8eb83
meca1 <drramos0@misena.edu.co>
2019-01-19 23:12:25 -0500

    Pagina terminada2

--- behavior.js
+++ behavior.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     var thumbnailElement = document.getElementById("smart_thumbnail");
     thumbnailElement.addEventListener("click", function () {
-        if (thumbnailElement.className == "img2") {
+        if (thumbnailElement.className == "small") {
 
             thumbnailElement.className = "";
         }
         else {
-            thumbnailElement.className = "img2";
+            thumbnailElement.className = "small";
         }
     });
 
